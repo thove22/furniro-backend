b44428c2d57af5742a57f6053fcc3adcbea6f574
thove22 <pedrostovela@gmail.com>
2026-07-04 00:47:35 +0200

    Initial Commit

---
+++ src/test/java/com/furniro/backend/FurniroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furniro.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurniroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
