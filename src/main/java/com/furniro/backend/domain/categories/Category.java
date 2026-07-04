9c8dcfaf31fdbe17f30a4413eb701dec2786342b
thove22 <pedrostovela@gmail.com>
2026-07-04 04:01:38 +0200

    feat: add Category domain entity

---
+++ src/main/java/com/furniro/backend/domain/categories/Category.java
@@ -0,0 +1,28 @@
+package com.furniro.backend.domain.categories;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Table (name = "Categories")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String imgUrl;
+
+    @CreationTimestamp
+    private Instant createdAt;
+}
