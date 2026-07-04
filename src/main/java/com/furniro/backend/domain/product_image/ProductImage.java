45b930afd40cc4b6816253f6da4a4e5bada53f75
thove22 <pedrostovela@gmail.com>
2026-07-04 04:04:14 +0200

    feat: add ProductImage domain entity

---
+++ src/main/java/com/furniro/backend/domain/product_image/ProductImage.java
@@ -0,0 +1,32 @@
+package com.furniro.backend.domain.product_image;
+
+import com.furniro.backend.domain.products.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Table(name = "product_images")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String url;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
