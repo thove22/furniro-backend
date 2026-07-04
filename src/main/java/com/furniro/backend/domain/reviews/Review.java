43515f56d149b7b2afcb23963c5f97531a59650d
thove22 <pedrostovela@gmail.com>
2026-07-04 04:05:32 +0200

    feat: add Review domain entity

---
+++ src/main/java/com/furniro/backend/domain/reviews/Review.java
@@ -0,0 +1,39 @@
+package com.furniro.backend.domain.reviews;
+
+import com.furniro.backend.domain.products.Product;
+import com.furniro.backend.domain.users.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Table (name = "reviews")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer rating;
+
+    @Column(columnDefinition = "text")
+    private String comment;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
