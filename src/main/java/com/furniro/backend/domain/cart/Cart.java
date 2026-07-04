e3672446e1f5bd852e243529e5e2608874edbdbb
thove22 <pedrostovela@gmail.com>
2026-07-04 04:04:45 +0200

    feat: add Cart domain entity

---
+++ src/main/java/com/furniro/backend/domain/cart/Cart.java
@@ -0,0 +1,34 @@
+package com.furniro.backend.domain.cart;
+
+import com.furniro.backend.domain.users.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Table(name = "cart")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
