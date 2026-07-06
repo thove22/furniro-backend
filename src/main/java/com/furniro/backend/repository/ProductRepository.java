d9c7c73840dd3fc11d03d87babad7a4236d21d33
thove22 <pedrostovela@gmail.com>
2026-07-06 13:46:42 +0200

    feat(repository): add ProductRepository

---
+++ src/main/java/com/furniro/backend/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.furniro.backend.repository;
+
+import com.furniro.backend.domain.products.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
