a07aefd7d97ed402f2e3c715c493701d8d1bad5a
thove22 <pedrostovela@gmail.com>
2026-07-06 13:45:52 +0200

    feat(repository): add ProductImageRepository

---
+++ src/main/java/com/furniro/backend/repository/ProductImageRepository.java
@@ -0,0 +1,9 @@
+package com.furniro.backend.repository;
+
+import com.furniro.backend.domain.product_image.ProductImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProductImageRepository extends JpaRepository<ProductImage, UUID> {
+}
